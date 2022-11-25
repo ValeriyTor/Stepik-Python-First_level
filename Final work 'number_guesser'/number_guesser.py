f29556e40a20677336343ac4947c8d3f7f853f9c
Valeriy Torgovanov <Faust_XVI@list.ru>
2022-11-25 17:15:31 +0200

    v 1.1
    game

---
+++ Final work 'number_guesser'/number_guesser.py
@@ -0,0 +1,61 @@
+from random import *
+
+phrases_too_much = ['Oh, too much! Try again', 'Too much!', "Whoa, that's too much!",
+                        'A lot!', 'Take it lower', 'Too much!', 'Need a smaller number!']
+phrases_too_little = ['Oh, too little! Try again', "It won't be enough!", "Oh, it's too little!",
+                        'Not enough!', 'Take it higher', 'Not enough!', 'Need more!']
+phrases_guessed = ['Congratulations! You guessed my number :)', 'Well done! You guessed it :)', 'Hooray, you guessed it! :)']
+
+def is_digit(num):
+    if num.isdigit():
+        return True
+    else:
+        return False
+
+flag = False
+print("Hello, my friend! Do yoy want to play with me?")
+print("Please, say 'Yes' or 'No'")
+print("I'm waiting...")
+answer = input("Answer is: ")
+while flag == False:
+    if (answer.lower() != 'yes' and answer.lower() != 'no'):
+        print("Whoa, wrong answer! Onle 'Yes' or 'No'! Try again!")
+        flag = False
+    elif answer.lower() == 'no':
+        print("Bad day! Ok! I understand! Se la vie!")
+        flag = True
+        break
+    else:
+        print("Wonderful choiсe! \n Let's start, but first of all you must choose upper bound! \n Please, tell me it:")
+        n = input("Upper bound is: ")
+        while True:
+            if is_digit(str(n)):
+                n = int(n)
+                break
+            else:
+                print("You didn't enter a number! Try again!")
+                n = input("Upper bound is: ")
+        secret_number = randint(1, n)
+        flag1 = False
+        print("I guessed a number from 0 to", n, "\nTry to guess!")
+        while flag1 == False:
+            my_number = input("Enter a number: ")
+            if is_digit(str(my_number)):
+                my_number = int(my_number)  
+                if my_number > secret_number:
+                    print(phrases_too_much[randint(0,len(phrases_too_much)-1)])
+                    print("Try again!")
+                    flag1 = False
+                elif my_number < secret_number:
+                    print(phrases_too_little[randint(0,len(phrases_too_little)-1)])
+                    print("Try again!")
+                    flag1 = False
+                else:
+                    print(phrases_guessed[randint(0,len(phrases_guessed)-1)])
+                    flag1 = True
+            else:
+                print("You didn't enter a number! Try again!")
+    print("-------------------------", "\nDo you want to play again?!")
+    print("Please, say 'Yes' or 'No'")
+    answer = input("Answer is: ")
+
---
+++ Final work 'number_guesser'/readme.txt
@@ -0,0 +1,13 @@
+Description of the program: get a random number in the detection from 1 to 100 and expect the user to guess this number. If the user's guess is greater than the random number, then the program should display the message 'Too much, try again'. If the guess is less than the random number, then the program should display the message 'Too small, try again'. If the user guesses the number, the program should congratulate him and display the message 'You guessed it, congratulations!'.
+
+Project components:
+
+    Integers (type int);
+    variables;
+    Data input / output (input() and print() functions);
+    Conditional statement (if/elif/else);
+    while loop;
+    Endless cycle;
+    statements break, continue;
+    Working with the random module to generate random numbers.
+
